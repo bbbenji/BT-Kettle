203863574acb1023226242d2b070beb3beeb2fb1
Benji <asdf.tgz@gmail.com>
2016-12-28 15:34:20 +0100

    Fixing mess

--- main.ino
+++ main.ino
@@ -1,25 +1,30 @@
-#include <SoftwareSerial.h>;
- 
-SoftwareSerial bluetooth(10, 11); // RX, TX int Power = 1; String receivedBluetoothString = 
-""; void setup() {
+#include <SoftSerial.h>
+#include <TinyPinChange.h>
+
+SoftSerial bluetooth(3, 4); // RX, TX
+int Power = 1;
+String receivedBluetoothString = "";
+
+void setup() {
   Serial.begin(9600);
   pinMode(Power,OUTPUT);
+  bluetooth.begin(57600); //??
 }
- 
+
 void loop() {
   while (bluetooth.available() > 0) {
- 
+
     char receivedBluetoothChar = bluetooth.read();
     receivedBluetoothString += receivedBluetoothChar;
- 
+
     if (receivedBluetoothChar == '\n') {
- 
+
       if (receivedBluetoothString.toInt() == 576965) {
         digitalWrite(Power, HIGH);
-        delay(100);
+        delay(5000);
         digitalWrite(Power, LOW);
       }
- 
+
       receivedBluetoothString = "";
     }
   }
